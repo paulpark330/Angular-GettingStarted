3e2e4d575cd7db3e7ce59e09aa2121effdec64a7
Paul Park <paulpark330@hotmail.com>
2021-09-23 18:02:26 -0700

    completed lesson 12

--- APM-Start/src/app/products/products-detail.component.ts
+++ APM-Start/src/app/products/products-detail.component.ts
@@ -1,14 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IProduct } from './product';
 
 @Component({
   templateUrl: './products-detail.component.html',
-  styleUrls: ['./products-detail.component.css']
+  styleUrls: ['./products-detail.component.css'],
 })
 export class ProductsDetailComponent implements OnInit {
-  pageTitle: string = 'Details'
-  constructor() { }
+  pageTitle: string = 'Product Details';
+  product: IProduct | undefined;
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.pageTitle += `: ${id}`;
+    this.product = {
+      productId: id,
+      productName: 'Leaf Rake',
+      productCode: 'GDN-0011',
+      releaseDate: 'March 19, 2021',
+      description: 'Leaf rake with 48-inch wooden handle.',
+      price: 19.95,
+      starRating: 3.2,
+      imageUrl: 'assets/images/leaf_rake.png',
+    };
   }
 
+  onBack(): void {
+    this.router.navigate(['/products']);
+  }
 }
